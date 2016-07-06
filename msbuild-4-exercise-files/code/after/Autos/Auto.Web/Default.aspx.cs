8dd54dff322e36087f86f64b915c19cc45da6aa1
Raj <rajesh@scmGalaxy.com>
2016-07-06 16:47:43 +0530

    First Commit

---
+++ msbuild-4-exercise-files/code/after/Autos/Auto.Web/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Automobiles;
+
+namespace Auto.Web
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+                return;
+
+            LoadAuto();
+        }
+
+        private void LoadAuto()
+        {
+             var mini = new MiniCooper();
+            _carName.Text = mini.Name;
+        }
+
+        
+    }
+}
